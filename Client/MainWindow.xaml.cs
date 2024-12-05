7fc3037105a732f1dabfd3b95815af5fac887f27
paranoe <is-21-4-6@permaviat.ru>
2024-12-05 15:13:49 +0500

    Написаны переменные. Дружинин

--- Client/MainWindow.xaml.cs
+++ Client/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Sockets;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -15,14 +17,16 @@ using System.Windows.Shapes;
 
 namespace Client
 {
-    /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        public static IPAddress IpAdress;
+        private Socket _clientSocket;
+        public static int Port;
+        public static int Id = -1;
         public MainWindow()
         {
             InitializeComponent();
         }
+
     }
 }
