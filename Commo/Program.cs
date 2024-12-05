5c797c27c16a40a072ac51c7120b6fc0acb7bd98
klipina53 <klipina53@gmail.com>
2024-12-05 15:17:34 +0500

    Прописанный метод AutorizationUser.Липина

--- Commo/Program.cs
+++ Commo/Program.cs
@@ -1,16 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Server
 {
-    public static List<User> Users = new List<User>();
-    public static IPAddress IAddress;
-    public static int Port;
+   
     public class Program
     {
+        public static List<User> Users = new List<User>();
+    public static IPAddress IAddress;
+    public static int Port;
+        public static bool AutorizationUser(string login,string password)
+        {
+            User user = null;
+            user = Users.Find(x=>x.login == login && x.password == password);
+            return user != null;
+        }
         static void Main(string[] args)
         {
         }
